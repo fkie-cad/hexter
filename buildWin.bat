225a0f44c10e5c0d07d4d8fa38e9c976d7d7aed0
Henning Braun <henning.braun@fkie.fraunhofer.de>
2019-07-26 16:47:07 +0200

    Updated build batch and Library inclusion.

--- buildWin.bat
+++ buildWin.bat
@@ -3,6 +3,7 @@
 set target=hexter
 set bitness=64
 set mode=Release
+set build_tools="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\"
 
 if not [%1]==[] (
     if [%1]==[/?] goto usage
@@ -17,9 +18,10 @@ if not [%3]==[] set mode=%3
 echo target=%target%
 echo bitness=%bitness%
 echo mode=%mode%
+echo build_tools
 
 :build
-    cmd /k "mkdir build & cd build & "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars%bitness%.bat" & cmake .. -DCMAKE_BUILD_TYPE=%mode% -G "CodeBlocks - NMake Makefiles" & cmake --build . --config %mode% --target %target%"
+    cmd /k "mkdir build & cd build & "%build_tools%\VC\Auxiliary\Build\vcvars%bitness%.bat" & cmake .. -DCMAKE_BUILD_TYPE=%mode% -G "CodeBlocks - NMake Makefiles" & cmake --build . --config %mode% --target %target%"
     exit /B 0
 
 :usage
