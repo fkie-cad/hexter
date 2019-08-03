6f16230b596451d2a3674b436be09a76ba17dea5
Henning Braun <henning.braun@fkie.fraunhofer.de>
2019-08-03 18:22:19 +0200

    Updated windows build batch.

--- buildWin.bat
+++ buildWin.bat
@@ -3,7 +3,7 @@
 set target=hexter
 set bitness=64
 set mode=Release
-set build_tools=C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\
+set buildTools="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\"
 
 if not [%1]==[] (
     if [%1]==[/?] goto usage
@@ -14,6 +14,7 @@ if not [%1]==[] (
 )
 if not [%2]==[] set bitness=%2
 if not [%3]==[] set mode=%3
+if not [%4]==[] set buildTools=%4
 
 set build_dir=build%bitness%
 
@@ -21,13 +22,13 @@ echo target=%target%
 echo bitness=%bitness%
 echo mode=%mode%
 echo build_dir=%build_dir%
-echo build_tools=%build_tools%
+echo buildTools=%buildTools%
 
 :build
-    cmd /k "mkdir %build_dir% & cd %build_dir% & "%build_tools%\VC\Auxiliary\Build\vcvars%bitness%.bat" & cmake .. -DCMAKE_BUILD_TYPE=%mode% -G "CodeBlocks - NMake Makefiles" & cmake --build . --config %mode% --target %target% & exit & cd .."
+    cmd /k "mkdir %build_dir% & cd %build_dir% & %buildTools%"\VC\Auxiliary\Build\vcvars%bitness%.bat" & cmake .. -DCMAKE_BUILD_TYPE=%mode% -G "CodeBlocks - NMake Makefiles" & cmake --build . --config %mode% --target %target% & exit & cd .."
     exit /B 0
 
 :usage
-    @echo Usage: %0 [hexter/hexter_shared [32/64 [Debug/Release]]]
-    @echo Default: %0 [hexter 64 Release]
+    @echo Usage: %0 [hexter/hexter_shared [32/64 [Debug/Release [buildTools]]]
+    @echo Default: %0 [%target% %bitness% %mode% %buildTools%]
     exit /B 1
