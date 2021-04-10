04e4659b04028d29d4ae7ddd7bcdb45a111b8195
Henning Braun <henning.braun@fkie.fraunhofer.de>
2021-04-10 20:04:39 +0200

    Fixed windows insert error

--- src/Writer.c
+++ src/Writer.c
@@ -11,9 +11,9 @@
 #include "utils/env.h"
 
 #if defined(_LINUX) || defined(__APPLE__)
-	#include <unistd.h>
+    #include <unistd.h>
 #elif defined(_WIN32)
-	#include <io.h>
+    #include <io.h>
 #endif
 
 #include "utils/common_fileio.h"
@@ -34,34 +34,34 @@ static void truncateFile(FILE* fp, size_t file_size, size_t ln);
  */
 uint32_t payloadParseByte(const char* arg, unsigned char** payload)
 {
-	int s;
-	uint32_t arg_ln = (uint32_t)strnlen(arg, MAX_PAYLOAD_LN);
-	if ( arg_ln < 1 )
-	{
-		printf("Error: Payload byte has no value!\n");
-		return 0;
-	}
-	if ( arg_ln > 2 )
-	{
-		printf("Error: Payload byte is too big!\n");
-		return 0;
-	}
-	arg_ln = 1;  // 1 byte
-	unsigned char* p = (unsigned char*) malloc(arg_ln);
-	if ( p == NULL )
-	{
-		printf("ERROR: Allocating memory failed!\n");
-		return 0;
-	}
+    int s;
+    uint32_t arg_ln = (uint32_t)strnlen(arg, MAX_PAYLOAD_LN);
+    if ( arg_ln < 1 )
+    {
+        printf("Error: Payload byte has no value!\n");
+        return 0;
+    }
+    if ( arg_ln > 2 )
+    {
+        printf("Error: Payload byte is too big!\n");
+        return 0;
+    }
+    arg_ln = 1;  // 1 byte
+    unsigned char* p = (unsigned char*) malloc(arg_ln);
+    if ( p == NULL )
+    {
+        printf("ERROR: Allocating memory failed!\n");
+        return 0;
+    }
 
-	s = parseUint8(&arg[0], p, 16);
-	if ( s != 0 )
-	{
-		return 0;
-	}
+    s = parseUint8(&arg[0], p, 16);
+    if ( s != 0 )
+    {
+        return 0;
+    }
 
-	*payload = p;
-	return arg_ln;
+    *payload = p;
+    return arg_ln;
 }
 
 /**
@@ -75,34 +75,34 @@ uint32_t payloadParseByte(const char* arg, unsigned char** payload)
  */
 uint32_t payloadParseFillBytes(const char* arg, unsigned char** payload, size_t ln)
 {
-	int s;
-	uint32_t arg_ln = (uint32_t)strnlen(arg, MAX_PAYLOAD_LN);
-	uint8_t fill_byte = 0;
-	if ( arg_ln < 1 )
-	{
-		printf("Error: Fill byte has no value!\n");
-		return 0;
-	}
-	if ( arg_ln > 2 )
-	{
-		printf("Error: Fill byte is too big!\n");
-		return 0;
-	}
-	arg_ln = (uint32_t)ln;
-	unsigned char* p = (unsigned char*) malloc(arg_ln);
-	if ( p == NULL )
-	{
-		printf("ERROR: Allocating memory failed!\n");
-		return 0;
-	}
+    int s;
+    uint32_t arg_ln = (uint32_t)strnlen(arg, MAX_PAYLOAD_LN);
+    uint8_t fill_byte = 0;
+    if ( arg_ln < 1 )
+    {
+        printf("Error: Fill byte has no value!\n");
+        return 0;
+    }
+    if ( arg_ln > 2 )
+    {
+        printf("Error: Fill byte is too big!\n");
+        return 0;
+    }
+    arg_ln = (uint32_t)ln;
+    unsigned char* p = (unsigned char*) malloc(arg_ln);
+    if ( p == NULL )
+    {
+        printf("ERROR: Allocating memory failed!\n");
+        return 0;
+    }
 
-	s = parseUint8(&arg[0], &fill_byte, 16);
-	if ( s != 0 )
-		return 0;
-	memset(p, fill_byte, arg_ln);
+    s = parseUint8(&arg[0], &fill_byte, 16);
+    if ( s != 0 )
+        return 0;
+    memset(p, fill_byte, arg_ln);
 
-	*payload = p;
-	return arg_ln;
+    *payload = p;
+    return arg_ln;
 }
 
 /**
@@ -115,36 +115,36 @@ uint32_t payloadParseFillBytes(const char* arg, unsigned char** payload, size_t 
  */
 uint32_t payloadParseWord(const char* arg, unsigned char** payload)
 {
-	int s;
-	uint32_t arg_ln = (uint32_t)strnlen(arg, MAX_PAYLOAD_LN);
-	if ( arg_ln < 1 )
-	{
-		printf("Error: Payload byte has no value!\n");
-		return 0;
-	}
-	if ( arg_ln > 4 )
-	{
-		printf("Error: Payload word is too big!\n");
-		return 0;
-	}
-	arg_ln = 2;  // 2 bytes
-	unsigned char* p = (unsigned char*) malloc(arg_ln);
-	if ( p == NULL )
-	{
-		printf("ERROR: Allocating memory failed!\n");
-		return 0;
-	}
+    int s;
+    uint32_t arg_ln = (uint32_t)strnlen(arg, MAX_PAYLOAD_LN);
+    if ( arg_ln < 1 )
+    {
+        printf("Error: Payload byte has no value!\n");
+        return 0;
+    }
+    if ( arg_ln > 4 )
+    {
+        printf("Error: Payload word is too big!\n");
+        return 0;
+    }
+    arg_ln = 2;  // 2 bytes
+    unsigned char* p = (unsigned char*) malloc(arg_ln);
+    if ( p == NULL )
+    {
+        printf("ERROR: Allocating memory failed!\n");
+        return 0;
+    }
 
-	uint16_t temp;
-	s = parseUint16(&arg[0], &temp, 16);
-	if ( s != 0 )
-		return 0;
+    uint16_t temp;
+    s = parseUint16(&arg[0], &temp, 16);
+    if ( s != 0 )
+        return 0;
 
-	// bytes are reversed using memcpy
-	memcpy(p, &temp, arg_ln);
+    // bytes are reversed using memcpy
+    memcpy(p, &temp, arg_ln);
 
-	*payload = p;
-	return arg_ln;
+    *payload = p;
+    return arg_ln;
 }
 
 /**
@@ -157,36 +157,36 @@ uint32_t payloadParseWord(const char* arg, unsigned char** payload)
  */
 uint32_t payloadParseDWord(const char* arg, unsigned char** payload)
 {
-	int s;
-	uint32_t arg_ln = (uint32_t)strnlen(arg, MAX_PAYLOAD_LN);
-	if ( arg_ln < 1 )
-	{
-		printf("Error: Payload byte has no value!\n");
-		return 0;
-	}
-	if ( arg_ln > 8 )
-	{
-		printf("Error: Payload double word is too big!\n");
-		return 0;
-	}
-	arg_ln = 4;  // 4 bytes
-	unsigned char* p = (unsigned char*) malloc(arg_ln);
-	if ( p == NULL )
-	{
-		printf("ERROR: Allocating memory failed!\n");
-		return 0;
-	}
+    int s;
+    uint32_t arg_ln = (uint32_t)strnlen(arg, MAX_PAYLOAD_LN);
+    if ( arg_ln < 1 )
+    {
+        printf("Error: Payload byte has no value!\n");
+        return 0;
+    }
+    if ( arg_ln > 8 )
+    {
+        printf("Error: Payload double word is too big!\n");
+        return 0;
+    }
+    arg_ln = 4;  // 4 bytes
+    unsigned char* p = (unsigned char*) malloc(arg_ln);
+    if ( p == NULL )
+    {
+        printf("ERROR: Allocating memory failed!\n");
+        return 0;
+    }
 
-	uint32_t temp;
-	s = parseUint32(&arg[0], &temp, 16);
-	if ( s != 0 )
-		return 0;
+    uint32_t temp;
+    s = parseUint32(&arg[0], &temp, 16);
+    if ( s != 0 )
+        return 0;
 
-	// bytes are reversed using memcpy
-	memcpy(p, &temp, arg_ln);
+    // bytes are reversed using memcpy
+    memcpy(p, &temp, arg_ln);
 
-	*payload = p;
-	return arg_ln;
+    *payload = p;
+    return arg_ln;
 }
 
 /**
@@ -199,36 +199,36 @@ uint32_t payloadParseDWord(const char* arg, unsigned char** payload)
  */
 uint32_t payloadParseQWord(const char* arg, unsigned char** payload)
 {
-	int s;
-	uint32_t arg_ln = (uint32_t)strnlen(arg, MAX_PAYLOAD_LN);
-	if ( arg_ln < 1 )
-	{
-		printf("Error: Payload byte has no value!\n");
-		return 0;
-	}
-	if ( arg_ln > 16 )
-	{
-		printf("Error: Payload quad word is too big!\n");
-		return 0;
-	}
-	arg_ln = 8;  // 8 bytes
-	unsigned char* p = (unsigned char*) malloc(arg_ln);
-	if ( p == NULL )
-	{
-		printf("ERROR: Allocating memory failed!\n");
-		return 0;
-	}
+    int s;
+    uint32_t arg_ln = (uint32_t)strnlen(arg, MAX_PAYLOAD_LN);
+    if ( arg_ln < 1 )
+    {
+        printf("Error: Payload byte has no value!\n");
+        return 0;
+    }
+    if ( arg_ln > 16 )
+    {
+        printf("Error: Payload quad word is too big!\n");
+        return 0;
+    }
+    arg_ln = 8;  // 8 bytes
+    unsigned char* p = (unsigned char*) malloc(arg_ln);
+    if ( p == NULL )
+    {
+        printf("ERROR: Allocating memory failed!\n");
+        return 0;
+    }
 
-	uint64_t temp;
-	s = parseUint64(&arg[0], &temp, 16);
-	if ( s != 0 )
-		return 0;
+    uint64_t temp;
+    s = parseUint64(&arg[0], &temp, 16);
+    if ( s != 0 )
+        return 0;
 
-	// bytes are reversed using memcpy
-	memcpy(p, &temp, arg_ln);
+    // bytes are reversed using memcpy
+    memcpy(p, &temp, arg_ln);
 
-	*payload = p;
-	return arg_ln;
+    *payload = p;
+    return arg_ln;
 }
 
 /**
@@ -241,27 +241,27 @@ uint32_t payloadParseQWord(const char* arg, unsigned char** payload)
  */
 uint32_t payloadParseUtf8(const char* arg, unsigned char** payload)
 {
-	uint32_t i;
-	uint32_t arg_ln = (uint32_t)strnlen(arg, MAX_PAYLOAD_LN);
-	if ( arg_ln < 1 )
-	{
-		printf("Error: Payload string has no value!\n");
-		return 0;
-	}
-	unsigned char* p = (unsigned char*) malloc(arg_ln);
-	if ( p == NULL )
-	{
-		printf("ERROR: Allocating memory failed!\n");
-		return 0;
-	}
+    uint32_t i;
+    uint32_t arg_ln = (uint32_t)strnlen(arg, MAX_PAYLOAD_LN);
+    if ( arg_ln < 1 )
+    {
+        printf("Error: Payload string has no value!\n");
+        return 0;
+    }
+    unsigned char* p = (unsigned char*) malloc(arg_ln);
+    if ( p == NULL )
+    {
+        printf("ERROR: Allocating memory failed!\n");
+        return 0;
+    }
 
-	for ( i = 0; i < arg_ln; i++ )
-	{
-		p[i] = (unsigned char) arg[i];
-	}
+    for ( i = 0; i < arg_ln; i++ )
+    {
+        p[i] = (unsigned char) arg[i];
+    }
 
-	*payload = p;
-	return arg_ln;
+    *payload = p;
+    return arg_ln;
 }
 
 /**
@@ -274,18 +274,18 @@ uint32_t payloadParseUtf8(const char* arg, unsigned char** payload)
  */
 uint32_t payloadParseUtf16(const char* arg, unsigned char** payload)
 {
-	uint32_t i;
+    uint32_t i;
     size_t arg_ln = (uint32_t)strnlen(arg, MAX_PAYLOAD_LN);
 
-	// fill buffer to get the real size
+    // fill buffer to get the real size
     unsigned char outb[MAX_PAYLOAD_LN*2] = {0};
     size_t outlen = MAX_PAYLOAD_LN*2;
 
-	if ( arg_ln < 1 )
-	{
-		printf("Error: Payload string has no value!\n");
-		return 0;
-	}
+    if ( arg_ln < 1 )
+    {
+        printf("Error: Payload string has no value!\n");
+        return 0;
+    }
 
     int s = UTF8ToUTF16LE(outb, &outlen, (unsigned char*)arg, &arg_ln);
 
@@ -296,21 +296,21 @@ uint32_t payloadParseUtf16(const char* arg, unsigned char** payload)
     }
 
     // alloc payload with real size
-	unsigned char* p = (unsigned char*) malloc(outlen);
-	if ( p == NULL )
-	{
-		printf("ERROR: Allocating memory failed!\n");
-		return 0;
-	}
+    unsigned char* p = (unsigned char*) malloc(outlen);
+    if ( p == NULL )
+    {
+        printf("ERROR: Allocating memory failed!\n");
+        return 0;
+    }
 
-	for ( i = 0; i < outlen; i++ )
-	{
-		p[i] = outb[i];
-	}
+    for ( i = 0; i < outlen; i++ )
+    {
+        p[i] = outb[i];
+    }
 
-	*payload = p;
+    *payload = p;
 
-	return (uint32_t)outlen;
+    return (uint32_t)outlen;
 }
 
 /**
@@ -323,21 +323,21 @@ uint32_t payloadParseUtf16(const char* arg, unsigned char** payload)
  */
 uint32_t payloadParseReversedPlainBytes(const char* arg, unsigned char** payload)
 {
-	uint32_t i, j;
-	unsigned char temp;
-	uint32_t payload_ln = payloadParsePlainBytes(arg, payload);
+    uint32_t i, j;
+    unsigned char temp;
+    uint32_t payload_ln = payloadParsePlainBytes(arg, payload);
 
-	for ( i = 0, j = payload_ln-1; i < payload_ln; i++, j-- )
-	{
-		if ( j <= i )
-			break;
+    for ( i = 0, j = payload_ln-1; i < payload_ln; i++, j-- )
+    {
+        if ( j <= i )
+            break;
 
-		temp = (*payload)[i];
-		(*payload)[i] = (*payload)[j];
-		(*payload)[j] = temp;
-	}
+        temp = (*payload)[i];
+        (*payload)[i] = (*payload)[j];
+        (*payload)[j] = temp;
+    }
 
-	return payload_ln;
+    return payload_ln;
 }
 
 /**
@@ -350,43 +350,43 @@ uint32_t payloadParseReversedPlainBytes(const char* arg, unsigned char** payload
  */
 uint32_t payloadParsePlainBytes(const char* arg, unsigned char** payload)
 {
-	uint32_t i, j;
-	uint16_t arg_ln = (uint16_t)strnlen(arg, MAX_PAYLOAD_LN);
-	unsigned char* p;
-	char byte[3] = {0};
-	uint32_t payload_ln;
-	int s = 0;
+    uint32_t i, j;
+    uint16_t arg_ln = (uint16_t)strnlen(arg, MAX_PAYLOAD_LN);
+    unsigned char* p;
+    char byte[3] = {0};
+    uint32_t payload_ln;
+    int s = 0;
 
-	if ( arg_ln % 2 != 0 || arg_ln == 0 )
-	{
-		printf("Error: Payload is not byte aligned!\n");
-		return 0;
-	}
+    if ( arg_ln % 2 != 0 || arg_ln == 0 )
+    {
+        printf("Error: Payload is not byte aligned!\n");
+        return 0;
+    }
 
-	p = (unsigned char*) malloc(arg_ln/2);
-	if ( p == NULL )
-	{
-		printf("ERROR: Allocating memory failed!\n");
-		return 0;
-	}
+    p = (unsigned char*) malloc(arg_ln/2);
+    if ( p == NULL )
+    {
+        printf("ERROR: Allocating memory failed!\n");
+        return 0;
+    }
 
-	for ( i = 0, j = 0; i < arg_ln; i += 2 )
-	{
-		byte[0] = arg[i];
-		byte[1] = arg[i + 1];
+    for ( i = 0, j = 0; i < arg_ln; i += 2 )
+    {
+        byte[0] = arg[i];
+        byte[1] = arg[i + 1];
 
-		 s = parseUint8(byte, &p[j++], 16);
-		 if ( s != 0 )
-		 {
-		 	free(p);
-		 	return 0;
-		 }
-	}
+         s = parseUint8(byte, &p[j++], 16);
+         if ( s != 0 )
+         {
+            free(p);
+            return 0;
+         }
+    }
 
-	payload_ln = arg_ln / 2;
+    payload_ln = arg_ln / 2;
 
-	*payload = p;
-	return payload_ln;
+    *payload = p;
+    return payload_ln;
 }
 
 /**
@@ -399,58 +399,58 @@ uint32_t payloadParsePlainBytes(const char* arg, unsigned char** payload)
  */
 void insert(const char* path, unsigned char* payload, uint32_t payload_ln, size_t offset)
 {
-	unsigned char buf[BLOCKSIZE_LARGE];
-	size_t n = BLOCKSIZE_LARGE;
-	FILE* fp;
-	size_t i, j;
+    unsigned char buf[BLOCKSIZE_LARGE];
+    size_t n = BLOCKSIZE_LARGE;
+    FILE* fp;
+    size_t i, j;
 
-	if ( offset > file_size )
-	{
-		overwrite(path, payload, payload_ln, offset);
-		return;
-	}
+    if ( offset > file_size )
+    {
+        overwrite(path, payload, payload_ln, offset);
+        return;
+    }
 
-	errno = 0;
-	fp = fopen(path, "rb+");
-	int errsv = errno;
-	if ( !fp )
-	{
-		printf("ERROR (0x%x): Could not open \"%s\".\n", errsv, path);
-		return;
-	}
+    errno = 0;
+    fp = fopen(path, "rb+");
+    int errsv = errno;
+    if ( !fp )
+    {
+        printf("ERROR (0x%x): Could not open \"%s\".\n", errsv, path);
+        return;
+    }
 
-	fseek(fp, offset, SEEK_SET);
-	while ( n == BLOCKSIZE_LARGE )
-	{
-		n = fread(buf, 1, BLOCKSIZE_LARGE, fp);
+    while ( n == BLOCKSIZE_LARGE )
+    {
+        fseek(fp, offset, SEEK_SET);
+        n = fread(buf, 1, BLOCKSIZE_LARGE, fp);
 
-		fseek(fp, offset, SEEK_SET);		// f: .....0123456789ABCDEF, buf = 0123456789ABCDEF, payload = DEAD0BEA
-		fwrite(payload, 1, payload_ln, fp); // f: .....DEAD0BEA89ABCDEF, buf = 0123456789ABCDEF, payload = DEAD0BEA
-		if ( n > payload_ln )
-		{
-			fwrite(buf, 1, n-payload_ln, fp);   // f: .....DEAD0BEA01234567, buf = 0123456789ABCDEF, payload = DEAD0BEA
+        fseek(fp, offset, SEEK_SET);		// f: .....0123456789ABCDEF, buf = 0123456789ABCDEF, payload = DEAD0BEA
+        fwrite(payload, 1, payload_ln, fp); // f: .....DEAD0BEA89ABCDEF, buf = 0123456789ABCDEF, payload = DEAD0BEA
+        if ( n > payload_ln )
+        {
+            fwrite(buf, 1, n-payload_ln, fp);   // f: .....DEAD0BEA01234567, buf = 0123456789ABCDEF, payload = DEAD0BEA
 
-			for ( i = n-payload_ln, j=0; i < n; i++ )
-			{
-				payload[j++] = buf[i]; // , buf = 0123456789ABCDEF, payload = 89ABCDEF
-			}
-		}
-		else
-		{
-			for ( i = 0; i < n; i++ )
-			{
-				payload[i] = buf[i];
-			}
-		}
+            for ( i = n-payload_ln, j=0; i < n; i++ )
+            {
+                payload[j++] = buf[i]; // , buf = 0123456789ABCDEF, payload = 89ABCDEF
+            }
+        }
+        else
+        {
+            for ( i = 0; i < n; i++ )
+            {
+                payload[i] = buf[i];
+            }
+        }
 
-		offset += n;
-	}
-	if ( n > payload_ln )
-		fwrite(payload, 1, payload_ln, fp);
-	else
-		fwrite(payload, 1, n, fp);
+        offset += n;
+    }
+    if ( n > payload_ln )
+        fwrite(payload, 1, payload_ln, fp);
+    else
+        fwrite(payload, 1, n, fp);
 
-	fclose(fp);
+    fclose(fp);
 }
 
 /**
@@ -463,25 +463,25 @@ void insert(const char* path, unsigned char* payload, uint32_t payload_ln, size_
  */
 void overwrite(const char* path, unsigned char* payload, uint32_t payload_ln, size_t offset)
 {
-	FILE* fp;
-	// backup
+    FILE* fp;
+    // backup
 //	FILE* bck;
 //	char buf[1024];
 //	int buf_ln = 1024;
 //	char dest_file_name[128];
 //	getTempFile(dest_file_name, "hexter.bck");
 //	int n = buf_ln;
-	// end backup
+    // end backup
 
-	errno = 0;
-	fp = fopen(path, "rb+");
-	int errsv = errno;
-	if ( !fp )
-	{
-		printf("ERROR (0x%x): Could not open \"%s\".\n", errsv, path);
-		return;
-	}
-	// backup
+    errno = 0;
+    fp = fopen(path, "rb+");
+    int errsv = errno;
+    if ( !fp )
+    {
+        printf("ERROR (0x%x): Could not open \"%s\".\n", errsv, path);
+        return;
+    }
+    // backup
 //	bck = fopen(dest_file_name, "wb");
 //	if ( !bck )
 //	{
@@ -495,12 +495,12 @@ void overwrite(const char* path, unsigned char* payload, uint32_t payload_ln, si
 //		fwrite(buf, 1, n, bck);
 //	}
 //	fclose(bck);
-	// end backup
+    // end backup
 
-	fseek(fp, offset, SEEK_SET);
-	fwrite(payload, 1, payload_ln, fp);
+    fseek(fp, offset, SEEK_SET);
+    fwrite(payload, 1, payload_ln, fp);
 
-	fclose(fp);
+    fclose(fp);
 }
 
 /**
@@ -512,67 +512,67 @@ void overwrite(const char* path, unsigned char* payload, uint32_t payload_ln, si
  */
 void deleteBytes(const char* path, size_t start, size_t ln)
 {
-	unsigned char buf[BLOCKSIZE_LARGE];
-	size_t n = BLOCKSIZE_LARGE;
-	FILE* fp;
-	size_t offset;
-	size_t end;
+    unsigned char buf[BLOCKSIZE_LARGE];
+    size_t n = BLOCKSIZE_LARGE;
+    FILE* fp;
+    size_t offset;
+    size_t end;
 
-	if ( start > file_size )
-		return;
+    if ( start > file_size )
+        return;
 
-	errno = 0;
-	fp = fopen(path, "rb+");
-	int errsv = errno;
-	if ( !fp )
-	{
-		printf("ERROR (0x%x): Could not open \"%s\".\n", errsv, path);
-		return;
-	}
+    errno = 0;
+    fp = fopen(path, "rb+");
+    int errsv = errno;
+    if ( !fp )
+    {
+        printf("ERROR (0x%x): Could not open \"%s\".\n", errsv, path);
+        return;
+    }
 
-	// If delete from start to end of file, just truncate.
-	if ( start + ln >= file_size )
-	{
-		ln = file_size - start;
-		truncateFile(fp, file_size, ln);
-		fclose(fp);
-		return;
-	}
+    // If delete from start to end of file, just truncate.
+    if ( start + ln >= file_size )
+    {
+        ln = file_size - start;
+        truncateFile(fp, file_size, ln);
+        fclose(fp);
+        return;
+    }
 
-	end = start + ln;
-	offset = end;
-	fseek(fp, offset, SEEK_SET);
-	while ( n == BLOCKSIZE_LARGE )
-	{
-		memset(buf, 0, BLOCKSIZE_LARGE);
+    end = start + ln;
+    offset = end;
+    fseek(fp, offset, SEEK_SET);
+    while ( n == BLOCKSIZE_LARGE )
+    {
+        memset(buf, 0, BLOCKSIZE_LARGE);
 
-		// read from offset
-		fseek(fp, offset, SEEK_SET);
-		n = fread(buf, 1, BLOCKSIZE_LARGE, fp);
+        // read from offset
+        fseek(fp, offset, SEEK_SET);
+        n = fread(buf, 1, BLOCKSIZE_LARGE, fp);
 
-		// write to start
-		fseek(fp, start, SEEK_SET);	 // f: ....0123456789ABCDEF, buf = 01234567, ln =
-		fwrite(buf, 1, n, fp);               // f: 01234567896789ABCDEF...., buf = 01234567
+        // write to start
+        fseek(fp, start, SEEK_SET);	 // f: ....0123456789ABCDEF, buf = 01234567, ln =
+        fwrite(buf, 1, n, fp);               // f: 01234567896789ABCDEF...., buf = 01234567
 
-		// increase offset and start
-		offset += n;
-		start += n;
-	}
+        // increase offset and start
+        offset += n;
+        start += n;
+    }
 
-	truncateFile(fp, file_size, ln);
+    truncateFile(fp, file_size, ln);
 
-	fclose(fp);
+    fclose(fp);
 }
 
 void truncateFile(FILE* fp, size_t size, size_t ln)
 {
 #if defined(_LINUX) || defined(__APPLE__)
-	ftruncate(fileno(fp), size-ln);
+    ftruncate(fileno(fp), size-ln);
 #elif defined(_WIN32)
-	#if defined(_32BIT)
-		_chsize(_fileno(fp), size-ln);
-	#else
-		_chsize_s(_fileno(fp), size-ln);
-	#endif
+    #if defined(_32BIT)
+        _chsize(_fileno(fp), size-ln);
+    #else
+        _chsize_s(_fileno(fp), size-ln);
+    #endif
 #endif
 }
